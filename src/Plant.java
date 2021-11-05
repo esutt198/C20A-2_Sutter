6203c15c3c3b1441589bf47c21b7fa565a1f2677
esutt198 <ehsutter@mines.edu>
2021-11-04 20:02:01 -0600

    Color now string

--- src/Plant.java
+++ src/Plant.java
@@ -3,21 +3,17 @@ import java.awt.Color;
 public class Plant {
 	String type;
 	int height;
-	Color color;
+	String color;
 	
 	public Plant(String type, int height, String colorStr) {
 		this.type = type;
 		this.height = height;
-		try {
-			this.color = (Color) Color.class.getDeclaredField(colorStr).get(null);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		this.color = color;
 		
 	}
 	
 	@Override
 	public String toString() {
-		return "Type: " + type + ", Height: " + Integer.toString(height) + " inches, Color: " + color.toString();
+		return "Type: " + type + ", Height: " + Integer.toString(height) + " inches, Color: " + color;
 	}
 }
