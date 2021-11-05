320c645c378961296641c2975f8698be93624eed
esutt198 <ehsutter@mines.edu>
2021-11-04 19:56:46 -0600

    Added rose

--- src/Greenhouse.java
+++ src/Greenhouse.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
 
 public class Greenhouse {
-
+	ArrayList<Plant> plants;
+	
+	public static void main(String[] args) {
+		Greenhouse g = new Greenhouse();
+		g.printPlants();
+	}
+	
+	public Greenhouse() {
+		plants = new ArrayList<Plant>();
+		addPlants();
+	}
+	
+	private void addPlants() {
+		Plant rose = new Plant("Rose", 24, "RED");
+		plants.add(rose);
+	}
+	
+	private void printPlants() {
+		System.out.println("The plants in the greenhouse are: ");
+		for(Plant p : plants) {
+			System.out.println(p);
+		}
+	}
 }
--- src/Plant.java
+++ src/Plant.java
@@ -1,3 +1,23 @@
-public class Plant {
+import java.awt.Color;
 
+public class Plant {
+	String type;
+	int height;
+	Color color;
+	
+	public Plant(String type, int height, String colorStr) {
+		this.type = type;
+		this.height = height;
+		try {
+			this.color = (Color) Color.class.getDeclaredField(colorStr).get(null);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Type: " + type + ", Height: " + Integer.toString(height) + " inches, Color: " + color.toString();
+	}
 }
